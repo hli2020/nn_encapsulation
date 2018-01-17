c7b549d6d60c639ceec4ae820d516554191d13b4
hli2020 <hongyangli@gmail.com>
2018-01-17 22:39:33 +0800

    update

---
+++ scripts/base_101_v5_3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+start=`date +%s`
+
+# train and test
+CUDA_VISIBLE_DEVICES=2 python holly_capsule.py \
+--experiment_name=base_101_v5_3 \
+--debug_mode=False \
+--dataset=cifar10 \
+--cap_model=v0 \
+--max_epoch=300 \
+--loss_form=margin \
+--optim=adam \
+--primary_cap_num=64 \
+--pre_ch_num=512 \
+--schedule 150 200 250 \
+--lr=0.0001
+
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Completed in" $runtime "seconds"
