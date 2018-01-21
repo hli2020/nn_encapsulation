8864d2f79b4a81bf3b08e1ac6e570b15f7c59ddb
hli2020 <hongyangli2020@gmail.com>
2018-01-21 10:15:41 +0800

    update

--- scripts/base_201_v1.sh
+++ scripts/base_201_v1.sh
@@ -4,18 +4,19 @@ start=`date +%s`
 
 #--experiment_name=base_101_v4_rerun \
 # train and test
-CUDA_VISIBLE_DEVICES=4,5 python holly_capsule.py \
+CUDA_VISIBLE_DEVICES=4 python holly_capsule.py \
 --experiment_name=base_201_v1 \
 --debug_mode=False \
 --dataset=tiny_imagenet \
 --setting=top1 \
 --cap_model=v0 \
+--route_num=2 \
 --max_epoch=500 \
 --loss_form=margin \
 --optim=adam \
 --schedule 200 300 400 \
---batch_size_train=64 \
---batch_size_test=64 \
+--batch_size_train=100 \
+--batch_size_test=100 \
 --primary_cap_num=32 \
 --pre_ch_num=256 \
 --lr=0.0001
