c017d46b7caee9e2293ece0a22a5f2c3aad3fefc
hli2020 <hongyangli2020@gmail.com>
2018-01-23 00:02:44 +0800

    update

--- run_analysis_stats.py
+++ run_analysis_stats.py
@@ -1,34 +1,50 @@
 from __future__ import print_function
-import torch.optim as optim
 import torch.utils.data as data
-from data.create_dset import create_dataset
 import torch.backends.cudnn as cudnn
-import torch.nn as nn
-from layers.modules.capsule import CapsNet
-from layers.modules.cap_layer import MarginLoss, SpreadLoss
-from layers.modules.cifar_train_val import *
-from utils.visualizer import Visualizer
-from utils.util import *
-from option.option import args   # for cifar we also has test here
+
+from layers.capsule import CapsNet
+from layers.train_val import test
+
+from data.create_dset import create_dataset
+from object_detection.utils.visualizer import Visualizer
+from object_detection.utils.util import *
+from option.option import Options
+
+cudnn.benchmark = True
+# config
+option = Options()
+option.setup_config()
+args = option.opt
 
 # ==============================
+# uncomment the following if you run in .sh file
 args.experiment_name = ''
 args.cap_model = 'v0'
+args.test_model_list = [1, 20, 80, 200, 300]
+
 args.draw_hist = True
 args.test_only = True
 args.non_target_j = False
-args.see_all_sample = True
-args.which_sample_index = 67   # only makes sense when 'see_all_sample' is false
-# ==============================
-if args.see_all_sample:
-    args.which_sample_index = -1
-args = show_jot_opt(args)
-vis = Visualizer(args)
 
-test_loader = data.DataLoader(create_dataset(args, 'test'), args.test_batch,
-                              num_workers=args.num_workers, shuffle=False)
+args.look_into_details = False
+# ==============================
+
+# init
+show_jot_opt(args)
+visual = Visualizer(args)
+
+test_loader = data.DataLoader(create_dataset(args, 'test'), args.batch_size_test,
+                              num_workers=args.num_workers, shuffle=False, pin_memory=True)
+# model
 model = CapsNet(num_classes=test_loader.dataset.num_classes, opts=args)
-print_log(model, args.file_name)
+model = model.cuda()
+
+model_summary, param_num = torch_summarize(model)
+print_log(model_summary, args.file_name)
+print_log('Total param num # {:f} Mb'.format(param_num), args.file_name)
+
+
+
 # set loss
 if args.model_cifar == 'capsule':
     if args.use_CE_loss:
@@ -46,7 +62,7 @@ if args.use_cuda:
 cudnn.benchmark = True
 
 
-test_model_list = [1, 20, 80, 200, 300]
+
 for _, i in enumerate(test_model_list):
     model_file = \
         os.path.join(args.save_folder, 'epoch_{:d}.pth'.format(i))
