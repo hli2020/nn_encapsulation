fe05d8e1efd4dd7778962a9af5f78855746d7c8b
hli2020 <hongyangli2020@gmail.com>
2018-01-23 00:04:12 +0800

    udpate from s35

--- scripts/101/base_101_v4.sh
+++ scripts/101/base_101_v4.sh
@@ -3,17 +3,19 @@
 start=`date +%s`
 
 #--experiment_name=base_101_v4_rerun \
+# _v4_less_aug
 # train and test
-CUDA_VISIBLE_DEVICES=0 python holly_capsule.py \
---experiment_name=base_101_v4_less_aug \
+CUDA_VISIBLE_DEVICES=0,1 python holly_capsule.py \
+--experiment_name=base_101_v4_2gpu \
 --debug_mode=False \
 --dataset=cifar10 \
 --cap_model=v0 \
---max_epoch=300 \
+--max_epoch=600 \
 --loss_form=margin \
 --optim=adam \
 --schedule 150 200 250 \
---lr=0.0001
+--lr=0.0001 \
+--s35
 
 
 end=`date +%s`
