b6e3426cb9896e6690b91ec60858ba2634f58fc0
hli2020 <hongyangli2020@gmail.com>
2018-01-25 21:42:08 +0800

    update

--- scripts/102/base_102_v1_1.sh
+++ scripts/102/base_102_v1_1.sh
@@ -3,7 +3,7 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=0,1
+DEVICE_ID=4,5
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
 --experiment_name=base_102_v1_1 \
