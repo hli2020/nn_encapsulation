7d6448a42dde3f74a2cd9cfd1275cbc6173d1144
hli2020 <hongyangli2020@gmail.com>
2018-01-29 23:57:58 +0800

    udpate

--- scripts/102/base_102_v1_1.sh
+++ scripts/102/base_102_v1_1.sh
@@ -3,7 +3,7 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=4,5
+DEVICE_ID=6,7
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
 --experiment_name=base_102_v1_1 \
