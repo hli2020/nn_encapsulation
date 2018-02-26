9401ddb282a1d57d8ac35bb3c76c57806a80169d
hli2020 <hongyangli2020@gmail.com>
2018-02-26 11:26:39 +0800

    cached

--- scripts/cap_102/cap_102_v2.sh
+++ scripts/cap_102/cap_102_v2.sh
@@ -3,14 +3,14 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=2,3
+DEVICE_ID=2
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
 --experiment_name=cap_102_v2 \
 --connect_detail=all \
 --cap_model=v2 \
 --manner=1 \
---debug_mode=False \
+--debug_mode=True \
 --less_data_aug
 
 
