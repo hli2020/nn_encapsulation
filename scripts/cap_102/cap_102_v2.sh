f49cd8ddc896eaf722f2a0eec778812009153ff9
hli2020 <hongyangli2020@gmail.com>
2018-02-26 11:38:11 +0800

    update from s35

--- scripts/cap_102/cap_102_v2.sh
+++ scripts/cap_102/cap_102_v2.sh
@@ -3,7 +3,7 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=2,3
+DEVICE_ID=0
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
 --experiment_name=cap_102_v2 \
@@ -11,7 +11,8 @@ CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --cap_model=v2 \
 --manner=1 \
 --debug_mode=False \
---less_data_aug
+--less_data_aug \
+--s35
 
 
 end=`date +%s`
