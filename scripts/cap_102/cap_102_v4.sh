3780f8c9bcf198f0ac9a3c32771a60e6ca79b7b3
hli2020 <hongyangli2020@gmail.com>
2018-02-28 16:52:30 +0800

    udpate from s35

--- scripts/cap_102/cap_102_v4.sh
+++ scripts/cap_102/cap_102_v4.sh
@@ -3,15 +3,16 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=0
+DEVICE_ID=3
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
---experiment_name=cap_102_v4 \
+--experiment_name=cap_102_v4_1gpu \
 --connect_detail=all \
 --cap_model=v2 \
 --manner=3 \
 --debug_mode=False \
---less_data_aug
+--less_data_aug \
+--s35
 
 
 end=`date +%s`
