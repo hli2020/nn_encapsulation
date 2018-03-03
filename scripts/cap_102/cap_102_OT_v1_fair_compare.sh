a7768fe142da688709564d8f502b8ef9ea5f096f
hli2020 <hongyangli2020@gmail.com>
2018-03-03 21:52:24 +0800

    update

--- scripts/cap_102/cap_102_OT_v1_fair_compare.sh
+++ scripts/cap_102/cap_102_OT_v1_fair_compare.sh
@@ -3,13 +3,13 @@
 start=`date +%s`
 
 # train and test
-DEVICE_ID=3
+DEVICE_ID=2
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
 --device_id=$DEVICE_ID \
---experiment_name=cap_102_OT_v1_fair_compare \
+--experiment_name=cap_102_OT_v1_fair_compare_new \
 --cap_model=v2 \
 --net_config=set_OT_compare \
---loss_fac=50 \
+--loss_fac=1 \
 --debug_mode=False \
 --less_data_aug \
 
