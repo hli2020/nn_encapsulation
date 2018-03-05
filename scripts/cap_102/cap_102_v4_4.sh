e65511488973254bcc07488193b7a197645079d3
hli2020 <hongyangli2020@gmail.com>
2018-03-05 17:16:27 +0800

    udpate

---
+++ scripts/cap_102/cap_102_v4_4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+start=`date +%s`
+
+# train and test
+DEVICE_ID=7
+CUDA_VISIBLE_DEVICES=$DEVICE_ID python holly_capsule.py \
+--device_id=$DEVICE_ID \
+--experiment_name=cap_102_v4_4 \
+--connect_detail=all \
+--cap_model=v2 \
+--manner=0 \
+--wider \
+--debug_mode=False \
+--less_data_aug \
+--s35
+
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Completed in" $runtime "seconds"
